644712e3ce7eb0a210f40a81f6e632c605a70bc6
Stefan Schuh <schuh@cs.uni-saarland.de>
2013-06-22 20:02:34 +0200

    Bessere Ausgabe fürs Protokoll
    noch mit auskommentieren, muss natürlich mit commanline flag realisiert
    werden. Das sollte dann mit argparse passieren

--- irv.py
+++ irv.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import sys
+import sys,argparse
 
 def bestremaining(remainingCandidates, vote):
     for c in vote:
@@ -29,27 +29,32 @@ def breakTie(tiedCandidates):
     return [tiedCandidates[0]]
 
 def printStack(stack):
-    for candidate in stack:
-        print str(candidate) + " hat "+ str(len(stack[candidate]))+" Stimmen auf seinem Stapel"
-        for pseudoId in stack[candidate]:
-            print str(pseudoId)+" : "+str(stack[candidate][pseudoId])
+    for candidate in sorted(stack):
+        if candidate==None:
+            print "Insgesamt sind "+ str(len(stack[candidate]))+" Stimmzettel auf keinem Stapel."
+        else:
+            print "Kandidat "+str(candidate) + " hat "+ str(len(stack[candidate]))+" Stimmen auf seinem Stapel."
+        #for pseudoId in stack[candidate]:
+         #   print str(pseudoId)+" : "+str(stack[candidate][pseudoId])
 
 
 def findWinner(candidates,votes):
+    runde = 1
     while (1):
         stacks=redistributeVotes(candidates, votes)
+        print "Die Verteilung der Stimmen in der "+str(runde)+". Runde:"
         printStack(stacks)
         numberVotes = [len(stacks[x]) for x in candidates]
         if max(numberVotes) >= required:
             winner = filter(lambda x: len(stacks[x])>= required, candidates)[0]
-            print "Mehr als 50% der Stimmen hat Kandidat " + winner+" auf seinem Stapel und gewinnt somit die Wahl."
             return winner 
-        print numberVotes
+        #print numberVotes
         minimum = min(numberVotes)
         tiedCandidates = filter(lambda x: len(stacks[x])==minimum , candidates)
         toBeRemoved = breakTie(tiedCandidates)
-        print "Der Stapel des Kandidaten " + str(toBeRemoved)+" wird aufgelöst."
+        print "Der Stapel des Kandidaten " + str(toBeRemoved[0])+u" wird aufgelöst."
         candidates = filter (lambda x: x not in toBeRemoved,candidates)
+        runde=runde+1
 
 if not sys.argv[1:]: 
     sys.argv += ["test-data/LL_Top3_IRV#01", "3"] 
@@ -69,12 +74,12 @@ for l in f:
 f.close()
 
 #print len(votes)
-print votes
+#print votes
 
 print "Es wurden insgesammt " + str(len(votes))+" Stimmen abgegeben."
 
 required = len(votes)/2 + 1
-print "Es werden somit mindestens " + str(required)+" Stimmen benötigt um gewählt zu werden."
+print "Es werden somit mindestens " + str(required)+u" Stimmen benötigt um gewählt zu werden."
 
 acceptance = {}
 for vote in votes.values():
@@ -92,12 +97,13 @@ print "Kandidaten die mehr als 50% der Stimmen haben: "+str(remainingCandidates)
 
 numberOfPositions = min(numberOfPositions , len(remainingCandidates))
 
-print "Somit werden in dieser Wahl "+str(numberOfPositions)+" Personen gewählt."
+print "Somit werden in dieser Wahl "+str(numberOfPositions)+u" Personen gewählt."
 
 
 gewinner = []
 for i in range(numberOfPositions):
     winner = findWinner(remainingCandidates, votes)
+    print "Kandidat " + winner+u" hat mehr als 50% der Stimmen auf seinem Stapel und gewinnt somit die Wahl für den "+str(i+1)+". Posten."
     gewinner.append(winner)
     remainingCandidates.remove(winner)
 

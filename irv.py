cb2e91e92ef508d1452ee7544266fffe1c46be2e
Stefan Schuh <stefan.schuh@gmail.com>
2013-11-09 17:15:43 +0100

    close #1: Protokoll Output in Wiki Syntax http://github.com/StefanSchuh/irv/issues/issue/1
    Sollte jetzt etwas hübscher anzusehen sein.

--- irv.py
+++ irv.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
-import sys,argparse
+import argparse
+import sys
 
+def printAndProtokoll(msg):
+    print msg
+    out.write(msg+"\n")
+    
+def prettyListString(list):
+    res = str(list[0])
+    for e in list[1:]:
+        res+=", "+str(e)
+        
+    return res
+    
 def bestremaining(remainingCandidates, vote):
     for c in vote:
         if c in remainingCandidates:
@@ -15,12 +27,13 @@ def redistributeVotes(remainingCandidates, votes):
     return stacks
 
 def askForTieBreak(tiedCandidates):
-	print "Folgende Kandidaten sind gleich auf: " 
-	print tiedCandidates
-	c = None
-	while (c not in tiedCandidates):
-		c = raw_input("Wessen Stapel soll aufgelöst werden?\n")
-	return [c]
+    printAndProtokoll("Folgende Kandidaten sind gleich auf: "+prettyListString(tiedCandidates)+".")
+    c = None
+    while (c not in tiedCandidates):
+        c = raw_input("Wessen Stapel soll aufgelöst werden?\n")
+    
+    printAndProtokoll("Kandidat "+str(c)+" wurde ausgelost und scheidet somit aus.")
+    return [c]
 
 
 def breakTie(tiedCandidates):
@@ -31,9 +44,9 @@ def breakTie(tiedCandidates):
 def printStack(stack):
     for candidate in sorted(stack):
         if candidate==None:
-            print "Insgesamt sind "+ str(len(stack[candidate]))+" Stimmzettel auf keinem Stapel."
+            printAndProtokoll( "Insgesamt sind "+ str(len(stack[candidate]))+" Stimmzettel aus der Wahl ausgeschieden.")
         else:
-            print "Kandidat "+str(candidate) + " hat "+ str(len(stack[candidate]))+" Stimmen auf seinem Stapel."
+            printAndProtokoll( "Kandidat "+str(candidate) + " hat "+ str(len(stack[candidate]))+" Stimmen auf seinem Stapel.")
         #for pseudoId in stack[candidate]:
          #   print str(pseudoId)+" : "+str(stack[candidate][pseudoId])
 
@@ -42,7 +55,7 @@ def findWinner(candidates,votes):
     runde = 1
     while (1):
         stacks=redistributeVotes(candidates, votes)
-        print "Die Verteilung der Stimmen in der "+str(runde)+". Runde:"
+        printAndProtokoll("Die Verteilung der Stimmen in der "+str(runde)+". Runde sieht wie folgt aus:")
         printStack(stacks)
         numberVotes = [len(stacks[x]) for x in candidates]
         if max(numberVotes) >= required:
@@ -52,9 +65,11 @@ def findWinner(candidates,votes):
         minimum = min(numberVotes)
         tiedCandidates = filter(lambda x: len(stacks[x])==minimum , candidates)
         toBeRemoved = breakTie(tiedCandidates)
-        print "Der Stapel des Kandidaten " + str(toBeRemoved[0])+u" wird aufgelöst."
+        printAndProtokoll( "Der Stapel des Kandidaten " + str(toBeRemoved[0])+u" wird aufgelöst.")
         candidates = filter (lambda x: x not in toBeRemoved,candidates)
         runde=runde+1
+
+
 
 if not sys.argv[1:]: 
     sys.argv += ["test-data/LL_Top3_IRV#01", "3"] 
@@ -63,7 +78,19 @@ if not sys.argv[1:]:
 fileName = sys.argv[1] #"test-data/test.dat"
 numberOfPositions = int(sys.argv[2]) # 2
 
+if not sys.argv[3:]:
+    sys.argv +=[fileName+".out"]
+
+
+outName = sys.argv[3]
+
+if not sys.argv[4:]:
+    amtName= raw_input("Welches Amt soll besetzt werden?")
+else:
+    amtName = sys.argv[4]
+
 f = open(fileName,'r')
+out = open(outName,'w')
 votes = {}
 i=1
 
@@ -76,40 +103,54 @@ f.close()
 #print len(votes)
 #print votes
 
-print "Es wurden insgesammt " + str(len(votes))+" Stimmen abgegeben."
-
-required = len(votes)/2 + 1
-print "Es werden somit mindestens " + str(required)+u" Stimmen benötigt um gewählt zu werden."
 
 acceptance = {}
 for vote in votes.values():
     for c in vote:
         acceptance[c]=acceptance.setdefault(c,0)+1
         
-print "Ergebnis der Akzeptanzwahl:"
+        
+
 candidates = sorted(acceptance.keys())
+printAndProtokoll("Es stehen "+ str(len(candidates))+" Kandidaten zur Wahl.")
 for c in candidates:
-	print str(c) + " " + str(acceptance[c])
+    printAndProtokoll("Kandidat "+str(c)+": ")
+
+
+printAndProtokoll("Es wurden insgesamt " + str(len(votes))+" Stimmen abgegeben.")
+
+required = len(votes)/2 + 1
+
+printAndProtokoll("Somit werden mindestens " + str(required)+u" Stimmen benötigt um gewählt zu werden.")
+
+
+
+printAndProtokoll("Ergebnis der Akzeptanzwahl:")
+
+for c in candidates:
+	printAndProtokoll (str(c) + " " + str(acceptance[c]))
 
 remainingCandidates = filter(lambda x: acceptance[x] >= required, candidates)
 
-print "Kandidaten die mehr als 50% der Stimmen haben: "+str(remainingCandidates)
+printAndProtokoll("Kandidaten die mehr als 50% der Stimmen haben: "+prettyListString(remainingCandidates))
 
 numberOfPositions = min(numberOfPositions , len(remainingCandidates))
 
-print "Somit werden in dieser Wahl "+str(numberOfPositions)+u" Personen gewählt."
+if numberOfPositions>1:
+    printAndProtokoll("Somit werden in dieser Wahl "+str(numberOfPositions)+u" Personen gewählt.")
 
 
 gewinner = []
 for i in range(numberOfPositions):
+    printAndProtokoll("\n==Wahl des "+str(i+1)+". "+amtName+"==\n")
     winner = findWinner(remainingCandidates, votes)
-    print "Kandidat " + winner+u" hat mehr als 50% der Stimmen auf seinem Stapel und gewinnt somit die Wahl für den "+str(i+1)+". Posten."
+    printAndProtokoll("Kandidat " + winner+u" hat mehr als 50% der Stimmen auf seinem Stapel und gewinnt somit die Wahl zum "+str(i+1)+". "+amtName)
     gewinner.append(winner)
     remainingCandidates.remove(winner)
 
-print "Folgende Kandidaten haben die Wahl gewonnen: "+ str(gewinner)
+print "Folgende Kandidaten haben die Wahl gewonnen: "+ prettyListString(gewinner)
     
-
+out.close()
 
 
 
